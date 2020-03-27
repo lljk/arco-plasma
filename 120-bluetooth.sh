dc8f31c6f0945a01cc1a0db846d5594e9de0bcc4
j. kaiden <jakekaiden@gmail.com>
2020-03-27 13:19:36 +0100

    Update and rename 120-bluetooth-v2.sh to 120-bluetooth.sh

--- 120-bluetooth.sh
+++ 120-bluetooth.sh
@@ -21,7 +21,7 @@ sudo pacman -S --noconfirm --needed pulseaudio-bluetooth
 sudo pacman -S --noconfirm --needed bluez
 sudo pacman -S --noconfirm --needed bluez-libs
 sudo pacman -S --noconfirm --needed bluez-utils
-sudo pacman -S --noconfirm --needed blueberry
+#sudo pacman -S --noconfirm --needed blueberry
 
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
